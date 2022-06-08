7879a5a81de7cbde081ba89568814e3b2830d37d
Harunnibal <56154614+harunc@users.noreply.github.com>
2022-06-08 23:51:29 +0300

    ReviewPage Small excercises on React
    ReviewPage Small excercises on React

---
+++ rewievPage/src/Review.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index,setIndex]=useState(0);
+  const {name,job,image,text}=people[index]
+
+  const checkNumber = (number)=>{
+    if (number<0){
+      number=people.length-1
+      return number
+    }
+    if(number>people.length-1){
+      number=0
+      return number
+    }
+    return number
+  }
+  const nextPerson = () =>{
+    setIndex((index)=>{
+      let newIndex=index+1
+      return checkNumber(newIndex)
+    })
+  }
+  const prevPerson = () =>{
+    setIndex(checkNumber(index-1))
+  }
+
+  const randomPerson =()=>{
+    let randomNum=Math.floor(Math.random()*people.length)
+    if (randomNum===index){
+      randomNum=checkNumber(randomNum+1)
+    }
+    setIndex(()=>{
+      let newIndex = randomNum
+      return newIndex
+    })
+
+  }
+  return <article className="review">
+<div className="img-container">
+ <img src={image} alt={name} className='person-img'/> 
+ <span className="quote-icon">
+   <FaQuoteRight/>
+ </span>
+</div>
+<h4 className="author">{name}</h4>
+<p className="job">{job}</p>
+<p className="info">{text}</p>
+<div className="button-container">
+<button className='prev-btn' onClick={prevPerson}>
+  <FaChevronLeft/>
+</button>
+<button className='next-btn' onClick={nextPerson}>
+  <FaChevronRight/>
+</button>
+</div>
+<button className='random-btn' onClick={randomPerson}>
+  Random
+</button>
+  </article>
+};
+
+export default Review;
