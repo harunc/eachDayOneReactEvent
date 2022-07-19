63c066c399c85a15936a3b8c49e35b6d30addf0c
Harunnibal <56154614+harunc@users.noreply.github.com>
2022-07-19 11:56:34 +0300

    Elysium App
    This app is an ecommerce website project. I haven't added payment method yet. I have used Sanity for Backend works.

---
+++ elysium-app/components/Cart.js
@@ -0,0 +1,71 @@
+import React,{useRef} from 'react'
+import Link from 'next/link';
+import {AiOutlineMinus,AiOutlinePlus,AiOutlineLeft,AiOutlineShopping} from 'react-icons/ai'
+import {TiDeleteOutline} from 'react-icons/ti'
+import { toast } from 'react-hot-toast';
+import {useStateContext} from '../context/StateContext'
+import {urlFor} from '../lib/client'
+
+const Cart = () => {
+  const cartRef= useRef();
+  const{onRemove,toggleCartItemQuentity,totalPrice,totalQuantities,cartItems,setshowCart}=useStateContext();
+  return (
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className="cart-container">
+        <button type='button' onClick={()=>setshowCart(false)} className='cart-heading'>
+          <AiOutlineLeft/>
+          <span className='heading'>Your Cart</span>
+          <span className='cart-num-items'>{totalQuantities} Items</span>
+        </button>
+        {cartItems.length<1 && (
+          <div className='empty-cart'>
+            <AiOutlineShopping size={150}/>
+            <h3>Empty Cart</h3>
+            <Link href="/">
+              <button type='button' onClick={()=>setshowCart(false)} className="btn">Continue Shopping</button>
+            </Link>
+           </div> 
+        )}
+        <div className="product-container">
+          {cartItems.length>=1 && cartItems.map((item)=>(
+            <div key={item._id} className="product">
+              <img src={urlFor(item?.image[0])} className="cart-product-image"/>
+              <div className="item-desc">
+                <div className="flex top">
+                  <h5>{item.name}</h5>
+                  <h4>${item.price}</h4>
+                </div>
+                <div className="flex bottom">
+                  <div>
+                    <p className="quantity-desc">
+              <span className="minus" onClick={()=>toggleCartItemQuentity(item._id,'dec')}><AiOutlineMinus /></span>
+              <span className="num">{item.quantity}</span>
+              <span className="plus" onClick={()=>toggleCartItemQuentity(item._id,'inc')}><AiOutlinePlus /></span>
+            </p>
+                  </div>
+                  <button type='button' className='remove-item' onClick={()=> onRemove(item)}><TiDeleteOutline/></button>
+                </div>
+              </div>
+            </div>
+          )
+          )}
+        </div>
+        {cartItems.length>=1 && (
+          <div className='cart-bottom'>
+            <div className="total">
+              <h3>Subtotal</h3>
+              <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <Link href="/success">
+              <button type='button' className='btn' onClick={()=>setshowCart(false)}>Pay With Stripe</button>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Cart
